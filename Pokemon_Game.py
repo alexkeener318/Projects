7e5773b8a843f197bd1a9ffe76ae1239d980171a
alexkeener318 <90213314+alexkeener318@users.noreply.github.com>
2021-09-07 13:26:59 -0500

    Update Pokemon_Game.py

--- Pokemon_Game.py
+++ Pokemon_Game.py
@@ -1,11 +1,4 @@
-# By submitting this assignment, I agree to the following:
-#  "Aggies do not lie, cheat, or steal, or tolerate those who do"
-#  "I have not given or received any unauthorized aid on this assignment"
-#
-# Name:       Alex Keener, Sean Kelly, Andrew Kinnick
-# Section:     515
-# Assignment:  Lab 13 - Activity 1
-# Date:        20 11  2020
+
 
 import csv
 from random import randint
